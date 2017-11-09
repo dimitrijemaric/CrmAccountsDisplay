b5b84f078f3442dac69ad219daab92e3c72f82d0
Dimitrije Maric <Dimitrije.Maric@endava.com>
2017-11-09 14:41:54 +0100

    Add project files.

---
+++ AccountInfo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AccountInfo.Startup))]
+namespace AccountInfo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
